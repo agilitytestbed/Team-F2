00a12b4c5702566d41cd7da67a303acb55aa3fc2
oplosthee <omniihd@gmail.com>
2018-02-13 22:51:45 +0100

    Add support for connecting to SQLite databases
    This commit adds the SQLite JDBC driver to the project's dependencies and adds a way to connect to the SQLite database which can be found in the project's resources.

---
+++ src/main/java/com/teamf/paymentassistant/database/DBConnection.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018, Joost Prins <github.com/joostprins>, Tom Leemreize <https://github.com/oplosthee>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.teamf.paymentassistant.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	/**
+	 * SQL Connection to the database.
+	 */
+	private Connection connection;
+
+	/**
+	 * Creates a new connection to the local SQLite database.
+	 * Requires the SQLite database to exist.
+	 */
+	public DBConnection() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			String path = "jdbc:sqlite:" + this.getClass().getClassLoader().getResource("transactions.sqlite").getPath();
+			this.connection = DriverManager.getConnection(path);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Returns an initialized connection to the SQLite database.
+	 *
+	 * @return connection to the SQLite database
+	 */
+	public Connection getConnection() {
+		return connection;
+	}
+
+}
